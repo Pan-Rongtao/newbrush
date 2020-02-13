a5fba9f114d98aceedd8f437a8df40626265abf2
Pan-Rongtao <32365198+Pan-Rongtao@users.noreply.github.com>
2020-02-13 10:28:08 +0800

    finish testing PropertyAnimationUsingKeyFrames

--- examples/newbrush-tests/TestPropertyAnimationUsingKeyFrames.cpp
+++ examples/newbrush-tests/TestPropertyAnimationUsingKeyFrames.cpp
@@ -43,6 +43,31 @@ public:
 		static auto dp = DependencyProperty::registerDependency<MyObject, double>("Height", 0.0);
 		return dp;
 	}
+	static DependencyProperty XProperty()
+	{
+		static auto dp = DependencyProperty::registerDependency<MyObject, Point>("X", Point());
+		return dp;
+	}
+	static DependencyProperty ActualSizeProperty()
+	{
+		static auto dp = DependencyProperty::registerDependency<MyObject, Size>("ActualSize", Size());
+		return dp;
+	}
+	static DependencyProperty FontColorProperty()
+	{
+		static auto dp = DependencyProperty::registerDependency<MyObject, Color>("FontColor", Color());
+		return dp;
+	}
+	static DependencyProperty RectProperty()
+	{
+		static auto dp = DependencyProperty::registerDependency<MyObject, Rect>("Rect", Rect());
+		return dp;
+	}
+	static DependencyProperty MargProperty()
+	{
+		static auto dp = DependencyProperty::registerDependency<MyObject, Thickness>("Marg", Thickness());
+		return dp;
+	}
 };
 
 TEST_CASE("test BoolAnimationUsingKeyFrames", "[BoolAnimationUsingKeyFrames]")
@@ -306,6 +331,176 @@ TEST_CASE("test DoubleAnimationUsingKeyFrames", "[DoubleAnimationUsingKeyFrames]
 		auto progress = tl->getCurrentProgress();
 		auto height = ui ? ui->get<double>(MyObject::HeightProperty()) : NAN;
 		printf("animation procesing: time[%s], progress[%.5f], count[%.5f]\n", time.toString().data(), progress, height);
+	};
+	animation.Completed += [](const EventArgs &args) {
+		printf("animation complete\n");
+	};
+	animation.begin();
+
+	while (true)
+	{
+		Timer::driveInLoop();
+	}
+}
+
+TEST_CASE("test PointAnimationUsingKeyFrames", "[PointAnimationUsingKeyFrames]")
+{
+	auto ui = std::make_shared<MyObject>();
+	PointAnimationUsingKeyFrames animation;
+	animation.setDuration(TimeSpan::fromSeconds(3));
+	animation.setTarget(ui);
+	animation.setTargetProperty(MyObject::XProperty());
+	animation.keyFrames().insert(PointKeyFrame(TimeSpan::fromSeconds(0), Point()));
+	animation.keyFrames().insert(PointKeyFrame(TimeSpan::fromSeconds(1), Point(100, 100)));
+	animation.keyFrames().insert(PointKeyFrame(TimeSpan::fromSeconds(2), Point(-50, 900)));
+
+	animation.StateChanged += [](const EventArgs &args) {
+		auto tl = static_cast<Timeline *>(args.sender);
+		auto state = tl->currentState();
+		printf("animation state chaged = %d\n", state);
+	};
+	animation.Process += [&ui](const EventArgs &args) {
+		auto tl = static_cast<Timeline *>(args.sender);
+		auto time = tl->getCurrentTime();
+		auto progress = tl->getCurrentProgress();
+		auto point = ui ? ui->get<Point>(MyObject::XProperty()) : Point();
+		printf("animation procesing: time[%s], progress[%.5f], point[%.f, %.f]\n", time.toString().data(), progress, point.x(), point.y());
+	};
+	animation.Completed += [](const EventArgs &args) {
+		printf("animation complete\n");
+	};
+	animation.begin();
+
+	while (true)
+	{
+		Timer::driveInLoop();
+	}
+}
+
+TEST_CASE("test SizeAnimationUsingKeyFrames", "[SizeAnimationUsingKeyFrames]")
+{
+	auto ui = std::make_shared<MyObject>();
+	SizeAnimationUsingKeyFrames animation;
+	animation.setDuration(TimeSpan::fromSeconds(3));
+	animation.setTarget(ui);
+	animation.setTargetProperty(MyObject::ActualSizeProperty());
+	animation.keyFrames().insert(SizeKeyFrame(TimeSpan::fromSeconds(0), Size()));
+	animation.keyFrames().insert(SizeKeyFrame(TimeSpan::fromSeconds(1), Size(100, 100)));
+	animation.keyFrames().insert(SizeKeyFrame(TimeSpan::fromSeconds(2), Size(-50, 900)));
+
+	animation.StateChanged += [](const EventArgs &args) {
+		auto tl = static_cast<Timeline *>(args.sender);
+		auto state = tl->currentState();
+		printf("animation state chaged = %d\n", state);
+	};
+	animation.Process += [&ui](const EventArgs &args) {
+		auto tl = static_cast<Timeline *>(args.sender);
+		auto time = tl->getCurrentTime();
+		auto progress = tl->getCurrentProgress();
+		auto size = ui ? ui->get<Size>(MyObject::ActualSizeProperty()) : Size();
+		printf("animation procesing: time[%s], progress[%.5f], size[%.f, %.f]\n", time.toString().data(), progress, size.width(), size.height());
+	};
+	animation.Completed += [](const EventArgs &args) {
+		printf("animation complete\n");
+	};
+	animation.begin();
+
+	while (true)
+	{
+		Timer::driveInLoop();
+	}
+}
+
+TEST_CASE("test ColorAnimationUsingKeyFrames", "[ColorAnimationUsingKeyFrames]")
+{
+	auto ui = std::make_shared<MyObject>();
+	ColorAnimationUsingKeyFrames animation;
+	animation.setDuration(TimeSpan::fromSeconds(3));
+	animation.setTarget(ui);
+	animation.setTargetProperty(MyObject::FontColorProperty());
+	animation.keyFrames().insert(ColorKeyFrame(TimeSpan::fromSeconds(0), Color()));
+	animation.keyFrames().insert(ColorKeyFrame(TimeSpan::fromSeconds(1), Color(0, 125, 100)));
+	animation.keyFrames().insert(ColorKeyFrame(TimeSpan::fromSeconds(2), Color(255, 10, 233)));
+
+	animation.StateChanged += [](const EventArgs &args) {
+		auto tl = static_cast<Timeline *>(args.sender);
+		auto state = tl->currentState();
+		printf("animation state chaged = %d\n", state);
+	};
+	animation.Process += [&ui](const EventArgs &args) {
+		auto tl = static_cast<Timeline *>(args.sender);
+		auto time = tl->getCurrentTime();
+		auto progress = tl->getCurrentProgress();
+		auto color = ui ? ui->get<Color>(MyObject::FontColorProperty()) : Color();
+		printf("animation procesing: time[%s], progress[%.5f], color[%d, %d, %d]\n", time.toString().data(), progress, color.red(), color.green(), color.blue());
+	};
+	animation.Completed += [](const EventArgs &args) {
+		printf("animation complete\n");
+	};
+	animation.begin();
+
+	while (true)
+	{
+		Timer::driveInLoop();
+	}
+}
+
+TEST_CASE("test RectAnimationUsingKeyFrames", "[RectAnimationUsingKeyFrames]")
+{
+	auto ui = std::make_shared<MyObject>();
+	RectAnimationUsingKeyFrames animation;
+	animation.setDuration(TimeSpan::fromSeconds(3));
+	animation.setTarget(ui);
+	animation.setTargetProperty(MyObject::RectProperty());
+	animation.keyFrames().insert(RectKeyFrame(TimeSpan::fromSeconds(0), Rect()));
+	animation.keyFrames().insert(RectKeyFrame(TimeSpan::fromSeconds(1), Rect(10, 10, 100, 50)));
+	animation.keyFrames().insert(RectKeyFrame(TimeSpan::fromSeconds(2), Rect(100, 100, 233, 233)));
+	
+	animation.StateChanged += [](const EventArgs &args) {
+		auto tl = static_cast<Timeline *>(args.sender);
+		auto state = tl->currentState();
+		printf("animation state chaged = %d\n", state);
+	};
+	animation.Process += [&ui](const EventArgs &args) {
+		auto tl = static_cast<Timeline *>(args.sender);
+		auto time = tl->getCurrentTime();
+		auto progress = tl->getCurrentProgress();
+		auto rc = ui ? ui->get<Rect>(MyObject::RectProperty()) : Rect();
+		printf("animation procesing: time[%s], progress[%.5f], color[%.f, %.f, %.f, %.f]\n", time.toString().data(), progress, rc.x(), rc.y(), rc.width(), rc.height());
+	};
+	animation.Completed += [](const EventArgs &args) {
+		printf("animation complete\n");
+	};
+	animation.begin();
+
+	while (true)
+	{
+		Timer::driveInLoop();
+	}
+}
+
+TEST_CASE("test ThicknessAnimationUsingKeyFrames", "[ThicknessAnimationUsingKeyFrames]")
+{
+	auto ui = std::make_shared<MyObject>();
+	ThicknessAnimationUsingKeyFrames animation;
+	animation.setDuration(TimeSpan::fromSeconds(3));
+	animation.setTarget(ui);
+	animation.setTargetProperty(MyObject::MargProperty());
+	animation.keyFrames().insert(ThicknessKeyFrame(TimeSpan::fromSeconds(0), Thickness()));
+	animation.keyFrames().insert(ThicknessKeyFrame(TimeSpan::fromSeconds(1), Thickness(10, 10, 100, 50)));
+	animation.keyFrames().insert(ThicknessKeyFrame(TimeSpan::fromSeconds(2), Thickness(100, 100, 233, 233)));
+
+	animation.StateChanged += [](const EventArgs &args) {
+		auto tl = static_cast<Timeline *>(args.sender);
+		auto state = tl->currentState();
+		printf("animation state chaged = %d\n", state);
+	};
+	animation.Process += [&ui](const EventArgs &args) {
+		auto tl = static_cast<Timeline *>(args.sender);
+		auto time = tl->getCurrentTime();
+		auto progress = tl->getCurrentProgress();
+		auto tn = ui ? ui->get<Thickness>(MyObject::MargProperty()) : Thickness();
+		printf("animation procesing: time[%s], progress[%.5f], color[%.f, %.f, %.f, %.f]\n", time.toString().data(), progress, tn.left, tn.top, tn.right, tn.bottom);
 	};
 	animation.Completed += [](const EventArgs &args) {
 		printf("animation complete\n");
--- include/newbrush/media/PropertyAnimationUsingKeyFrames.h
+++ include/newbrush/media/PropertyAnimationUsingKeyFrames.h
@@ -92,14 +92,14 @@ protected:
 		{
 			auto const &curFrame = *curFrameIter;
 			auto prevFrameIter = (curFrameIter == m_keyFrames.begin()) ? m_keyFrames.end() : --curFrameIter;
-			auto fromValue = (prevFrameIter == m_keyFrames.end()) ? curFrame.value() : (*prevFrameIter).value();
-			auto toValue = curFrame.value();
+			auto const &fromValue = (prevFrameIter == m_keyFrames.end()) ? curFrame.value() : (*prevFrameIter).value();
+			auto const &toValue = curFrame.value();
 			int64_t frameBegTick = (prevFrameIter == m_keyFrames.end() ? 0 : (int64_t)(*prevFrameIter).keyTime().totalMilliseconds());
 			int64_t frameEndTick = (int64_t)curFrame.keyTime().totalMilliseconds();
 			auto curFrameEasing = curFrame.easing() ? curFrame.easing() : std::make_shared<LinearEase>();
 			auto curFrameProgress = (double)(ticks - frameBegTick) / (frameEndTick - frameBegTick);
-			auto easingProgress = curFrameEasing->easeInCore(curFrameProgress);
-			auto value = static_cast<T>( fromValue + (toValue - fromValue) * easingProgress );
+			auto ft = (float)curFrameEasing->easeInCore(curFrameProgress);
+			auto value = static_cast<T>( fromValue + (toValue - fromValue) * ft );
 			target().lock()->set(targetProperty(), value);
 		}
 	}
@@ -157,85 +157,59 @@ template<> void PropertyAnimationUsingKeyFrames<std::string>::onProcessing()
 	}
 }
 
-template<> void PropertyAnimationUsingKeyFrames<Point>::onProcessing()
-{
-	if (!target().lock() || m_keyFrames.empty())
-		return;
-
-	auto progress = getCurrentProgress();
-	auto ticks = (int64_t)(duration().totalMilliseconds() * getCurrentProgress());
-	auto curFrameIter = getCurrentFrame(ticks);
-	if (curFrameIter != m_keyFrames.end())
-	{
-		auto curFrame = *curFrameIter;
-		auto prevFrameIter = (curFrameIter == m_keyFrames.begin()) ? m_keyFrames.end() : --curFrameIter;
-		auto fromValue = (prevFrameIter == m_keyFrames.end()) ? curFrame.value() : (*prevFrameIter).value();
-		auto toValue = curFrame.value();
-		auto curFrameEasing = curFrame.easing() ? curFrame.easing() : std::make_shared<LinearEase>();
-		auto easingProgress = curFrameEasing->easeInCore(progress);
-		//decltype(fromValue) value = fromValue + (toValue - fromValue) * easingProgress;
-		target().lock()->set(targetProperty(), Point());
-	}
-}
-template<> void PropertyAnimationUsingKeyFrames<Size>::onProcessing()
-{
-	if (!target().lock() || m_keyFrames.empty())
-		return;
-
-	auto progress = getCurrentProgress();
-	auto ticks = (int64_t)(duration().totalMilliseconds() * getCurrentProgress());
-	auto curFrameIter = getCurrentFrame(ticks);
-	if (curFrameIter != m_keyFrames.end())
-	{
-		auto curFrame = *curFrameIter;
-		auto prevFrameIter = (curFrameIter == m_keyFrames.begin()) ? m_keyFrames.end() : --curFrameIter;
-		auto fromValue = (prevFrameIter == m_keyFrames.end()) ? curFrame.value() : (*prevFrameIter).value();
-		auto toValue = curFrame.value();
-		auto curFrameEasing = curFrame.easing() ? curFrame.easing() : std::make_shared<LinearEase>();
-		auto easingProgress = curFrameEasing->easeInCore(progress);
-		//decltype(fromValue) value = fromValue + (toValue - fromValue) * easingProgress;
-		target().lock()->set(targetProperty(), Size());
-	}
-}
-template<> void PropertyAnimationUsingKeyFrames<Rect>::onProcessing()
-{
-	if (!target().lock() || m_keyFrames.empty())
-		return;
-
-	auto progress = getCurrentProgress();
-	auto ticks = (int64_t)(duration().totalMilliseconds() * getCurrentProgress());
-	auto curFrameIter = getCurrentFrame(ticks);
-	if (curFrameIter != m_keyFrames.end())
-	{
-		auto curFrame = *curFrameIter;
-		auto prevFrameIter = (curFrameIter == m_keyFrames.begin()) ? m_keyFrames.end() : --curFrameIter;
-		auto fromValue = (prevFrameIter == m_keyFrames.end()) ? curFrame.value() : (*prevFrameIter).value();
-		auto toValue = curFrame.value();
-		auto curFrameEasing = curFrame.easing() ? curFrame.easing() : std::make_shared<LinearEase>();
-		auto easingProgress = curFrameEasing->easeInCore(progress);
-		//decltype(fromValue) value = fromValue + (toValue - fromValue) * easingProgress;
-		target().lock()->set(targetProperty(), Rect());
-	}
-}
-
 template<> void PropertyAnimationUsingKeyFrames<Color>::onProcessing()
 {
 	if (!target().lock() || m_keyFrames.empty())
 		return;
 
 	auto progress = getCurrentProgress();
-	auto ticks = (int64_t)(duration().totalMilliseconds() * getCurrentProgress());
+	auto ticks = (int64_t)(duration().totalMilliseconds() * progress);
 	auto curFrameIter = getCurrentFrame(ticks);
 	if (curFrameIter != m_keyFrames.end())
 	{
-		auto curFrame = *curFrameIter;
+		auto const &curFrame = *curFrameIter;
 		auto prevFrameIter = (curFrameIter == m_keyFrames.begin()) ? m_keyFrames.end() : --curFrameIter;
-		auto fromValue = (prevFrameIter == m_keyFrames.end()) ? curFrame.value() : (*prevFrameIter).value();
-		auto toValue = curFrame.value();
+		auto const &fromValue = (prevFrameIter == m_keyFrames.end()) ? curFrame.value() : (*prevFrameIter).value();
+		auto const &toValue = curFrame.value();
+		int64_t frameBegTick = (prevFrameIter == m_keyFrames.end() ? 0 : (int64_t)(*prevFrameIter).keyTime().totalMilliseconds());
+		int64_t frameEndTick = (int64_t)curFrame.keyTime().totalMilliseconds();
 		auto curFrameEasing = curFrame.easing() ? curFrame.easing() : std::make_shared<LinearEase>();
-		auto easingProgress = curFrameEasing->easeInCore(progress);
-		//decltype(fromValue) value = fromValue + (toValue - fromValue) * easingProgress;
-		target().lock()->set(targetProperty(), Rect());
+		auto curFrameProgress = (double)(ticks - frameBegTick) / (frameEndTick - frameBegTick);
+		auto ft = curFrameEasing->easeInCore(curFrameProgress);
+		auto r = (int)((int)fromValue.red() + ft * ((int)toValue.red() - (int)fromValue.red()));
+		auto g = (int)((int)fromValue.green() + ft * ((int)toValue.green() - (int)fromValue.green()));
+		auto b = (int)((int)fromValue.blue() + ft * ((int)toValue.blue() - (int)fromValue.blue()));
+		r = clamp(0, 255, r);
+		g = clamp(0, 255, g);
+		b = clamp(0, 255, b);
+		target().lock()->set(targetProperty(), Color(r, g, b));
+	}
+}
+
+template<> void PropertyAnimationUsingKeyFrames<Rect>::onProcessing()
+{
+	if (!target().lock() || m_keyFrames.empty())
+		return;
+
+	auto progress = getCurrentProgress();
+	auto ticks = (int64_t)(duration().totalMilliseconds() * progress);
+	auto curFrameIter = getCurrentFrame(ticks);
+	if (curFrameIter != m_keyFrames.end())
+	{
+		auto const &curFrame = *curFrameIter;
+		auto prevFrameIter = (curFrameIter == m_keyFrames.begin()) ? m_keyFrames.end() : --curFrameIter;
+		auto const &fromValue = (prevFrameIter == m_keyFrames.end()) ? curFrame.value() : (*prevFrameIter).value();
+		auto const &toValue = curFrame.value();
+		int64_t frameBegTick = (prevFrameIter == m_keyFrames.end() ? 0 : (int64_t)(*prevFrameIter).keyTime().totalMilliseconds());
+		int64_t frameEndTick = (int64_t)curFrame.keyTime().totalMilliseconds();
+		auto curFrameEasing = curFrame.easing() ? curFrame.easing() : std::make_shared<LinearEase>();
+		auto curFrameProgress = (double)(ticks - frameBegTick) / (frameEndTick - frameBegTick);
+		auto ft = (float)curFrameEasing->easeInCore(curFrameProgress);
+		auto x = fromValue.x() + (toValue.x() - fromValue.x()) * ft;
+		auto y = fromValue.y() + (toValue.y() - fromValue.y()) * ft;
+		auto w = fromValue.width() + (toValue.width() - fromValue.width()) * ft;
+		auto h = fromValue.height() + (toValue.height() - fromValue.height()) * ft;
+		target().lock()->set(targetProperty(), Rect(x, y, w, h));
 	}
 }
 
@@ -245,18 +219,24 @@ template<> void PropertyAnimationUsingKeyFrames<Thickness>::onProcessing()
 		return;
 
 	auto progress = getCurrentProgress();
-	auto ticks = (int64_t)(duration().totalMilliseconds() * getCurrentProgress());
+	auto ticks = (int64_t)(duration().totalMilliseconds() * progress);
 	auto curFrameIter = getCurrentFrame(ticks);
 	if (curFrameIter != m_keyFrames.end())
 	{
-		auto curFrame = *curFrameIter;
+		auto const &curFrame = *curFrameIter;
 		auto prevFrameIter = (curFrameIter == m_keyFrames.begin()) ? m_keyFrames.end() : --curFrameIter;
-		auto fromValue = (prevFrameIter == m_keyFrames.end()) ? curFrame.value() : (*prevFrameIter).value();
-		auto toValue = curFrame.value();
+		auto const &fromValue = (prevFrameIter == m_keyFrames.end()) ? curFrame.value() : (*prevFrameIter).value();
+		auto const &toValue = curFrame.value();
+		int64_t frameBegTick = (prevFrameIter == m_keyFrames.end() ? 0 : (int64_t)(*prevFrameIter).keyTime().totalMilliseconds());
+		int64_t frameEndTick = (int64_t)curFrame.keyTime().totalMilliseconds();
 		auto curFrameEasing = curFrame.easing() ? curFrame.easing() : std::make_shared<LinearEase>();
-		auto easingProgress = curFrameEasing->easeInCore(progress);
-		//decltype(fromValue) value = fromValue + (toValue - fromValue) * easingProgress;
-		target().lock()->set(targetProperty(), Thickness());
+		auto curFrameProgress = (double)(ticks - frameBegTick) / (frameEndTick - frameBegTick);
+		auto ft = (float)curFrameEasing->easeInCore(curFrameProgress);
+		auto l = fromValue.left + (toValue.left - fromValue.left) * ft;
+		auto r = fromValue.right + (toValue.right - fromValue.right) * ft;
+		auto t = fromValue.top + (toValue.top - fromValue.top) * ft;
+		auto b = fromValue.bottom + (toValue.bottom - fromValue.bottom) * ft;
+		target().lock()->set(targetProperty(), Thickness(l, t, r, b));
 	}
 }
 
